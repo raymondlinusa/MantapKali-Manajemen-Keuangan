e7b175506b1805c24245441e439ae3a6c295ae71
raymondlinusa <raymond.linusa@ti.ukdw.ac.id>
2020-11-13 16:52:17 +0700

    initial commit

---
+++ app/src/main/java/com/example/uangmantapkali/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.uangmantapkali.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uangmantapkali.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
