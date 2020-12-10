a84a4a75eb2b72a6f5f573a183934b7112decc4b
Charles C <71957934+Cattosan@users.noreply.github.com>
2020-12-10 10:36:00 +0700

    Update Fraagment dan Button

--- app/src/main/java/com/example/uangmantapkali/ui/HomeActivity.java
+++ app/src/main/java/com/example/uangmantapkali/ui/HomeActivity.java
@@ -1,19 +1,24 @@
 package com.example.uangmantapkali.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.ui.AppBarConfiguration;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import com.example.uangmantapkali.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class HomeActivity extends AppCompatActivity {
 
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+    public static final int fl_container = 1000039;
     private Button logout;
 
     private FirebaseAuth mAuth;
@@ -26,6 +31,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
@@ -48,5 +56,32 @@ public class HomeActivity extends AppCompatActivity {
             Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;   switch (item.getItemId()){
+            case R.id.home_menu:
+                fragment = new HomeFragment();
+                break;
+            case R.id.income:
+                fragment = new IncomeFragment();
+                break;
+            case R.id.outcome:
+                fragment = new OutcomeFragment();
+                break;
+            case R.id.profile:
+                fragment = new AccountFragment();
+                break;
+        }   return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getChildFragmentManager().beginTransaction()
+                    .replace(fl_container, fragment)
+                    .commit();
+            return true;
+        }   return false;
     }
 }
